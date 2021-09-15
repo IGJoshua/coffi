25bbd243396ef6e8cdb699d3cd2cddb57b6f4a96
Joshua Suskalo <joshua@iris.tv>
2021-09-15 12:16:33 -0500

    Update to allow use of restricted functions in dev

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,1 @@
-((clojure-mode . ((cider-clojure-cli-global-options . "-A:test:dev -J--add-modules -Jjdk.incubator.foreign"))))
+((clojure-mode . ((cider-clojure-cli-global-options . "-A:test:dev -J--add-modules=jdk.incubator.foreign -J--enable-native-access=ALL-UNNAMED"))))
