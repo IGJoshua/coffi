8cd93eaf8efc141b59f5f380e202e18d05a93b58
Joshua Suskalo <joshua@iris.tv>
2021-09-16 09:41:09 -0500

    Add nodisassemble as a dev dependency, and move modules there too

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,1 @@
-((clojure-mode . ((cider-clojure-cli-global-options . "-A:test:dev -J--add-modules=jdk.incubator.foreign -J--enable-native-access=ALL-UNNAMED"))))
+((clojure-mode . ((cider-clojure-cli-global-options . "-A:test:dev -J-javaagent:$HOME/.m2/repository/nodisassemble/nodisassemble/0.1.3/nodisassemble-0.1.3.jar"))))
