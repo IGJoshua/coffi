673a6532003672d63c5b27edbfca3efd959aa813
Joshua Suskalo <joshua@iris.tv>
2021-10-01 10:39:32 -0500

    Fix bug with primitive-serializing arguments to downcalls not compiling

--- src/clj/coffi/ffi.clj
+++ src/clj/coffi/ffi.clj
@@ -108,9 +108,10 @@
 (defn- insn-layout
   "Gets the type keyword or class for referring to the type in bytecode."
   [type]
-  (if (some-> (mem/primitive-type type) (not= ::mem/pointer))
-    (keyword (name type))
-    (mem/java-layout type)))
+  (when-some [prim (mem/primitive-type type)]
+    (if (not= prim ::mem/pointer)
+      (keyword (name prim))
+      (mem/java-layout type))))
 
 (def ^:private unbox-fn-for-type
   "Map from type name to the name of its unboxing function."
